b1c0ddc2dfd2617ea2c1b68bf93935130ebf189a
Vivek Thakur <vivekthakur9810@gmail.com>
2023-09-18 18:16:47 +0530

    contact page changes

--- src/components/soumya/ContactUs.jsx
+++ src/components/soumya/ContactUs.jsx
@@ -17,11 +17,10 @@ const ContactUs = () => {
       </div>
       <div className="page flex">
       <div className="flex justify-center items-center">
-        <div className="box m-10">
-          <div className="container border border-gray-200 rounded-xl">
-            <div className=" ">
-              <div className="flex justify-between">
-                <div className="w-1/2">
+        <div className="box my-20 w-[95%] sm:w-4/5">
+          <div className="border border-gray-200 rounded-xl">
+            <div className="flex flex-wrap">
+                <div className="md:w-1/2 w-full min-h-[30rem] ">
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14023.698868352063!2d77.0342411732628!3d28.45949682438397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1e99c8b4e59d%3A0x7a64779c31d49e0a!2sGurgaon%2C%20Haryana!5e0!3m2!1sen!2sin!4v1687451404232!5m2!1sen!2sin"
                     width="100%"
@@ -31,7 +30,7 @@ const ContactUs = () => {
                     className="h-full"
                   ></iframe>
                 </div>
-                <div className="w-1/2 bg-white p-6">
+                <div className="w-full md:w-1/2 bg-white p-6 ">
                   <h1 className="text-3xl font-bold mt-4 mb-2 text-center text-black">
                     Get in
                     <span className="text-primary"> Touch</span>
@@ -98,7 +97,7 @@ const ContactUs = () => {
                           <div className='btn bg-primary hover:bg-white text-white border-primary hover:text-primary hover:border-primary text-center shadow-gray-300 shadow-md hover:shadow-2xl'>Submit</div>
                       </div>
                   </form>
-                  <div className="mt-3 flex justify-center items-center">
+                  <div className="mt-3 block sm:flex sm:justify-center sm:items-center">
                     <div className="mr-8">
                     <i class="fa-solid fa-envelope mr-2"></i>
                       hea@gmail.com
@@ -112,7 +111,6 @@ const ContactUs = () => {
                       www.hea.com
                     </div>
                   </div>
-                </div>
               </div>
             </div>
           </div>

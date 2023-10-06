cc67caf144449ed7115c8d19b1d08141d785b0a7
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-10-06 11:13:23 +0530

    changed api

--- src/redux/API.jsx
+++ src/redux/API.jsx
@@ -1,6 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: "https://hea-zg7o.onrender.com/",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://hea-zg7o.onrender.com/",
 });

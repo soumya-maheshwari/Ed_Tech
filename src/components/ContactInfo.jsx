85a3db62c1886b9488b00807bf1b35b53f683fd5
Vivek Thakur <vivekthakur9810@gmail.com>
2023-10-15 22:30:28 +0530

    course and contact info for admin added

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,4 +1,6 @@
 import React, { useState, useEffect } from "react";
+import NavBar from "./NavBar/NavBar";
+import { Footer } from "antd/es/layout/layout";
 
 const ContactInfo = () => {
   const [posts, setPosts] = useState([]);
@@ -7,14 +9,18 @@ const ContactInfo = () => {
   const [postsPerPage] = useState(10);
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    window.scroll(0, 0);
+  }, []);
+  useEffect(() => {
+    fetch("https://hea-zg7o.onrender.com/contact/allContacts")
       .then((response) => response.json())
       .then((data) => {
-        setPosts(data);
+        console.log("data  ", data);
+        setPosts([...data.contacts]);
         setLoading(false);
       });
   }, []);
-
+  console.log(posts);
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
@@ -22,16 +28,42 @@ const ContactInfo = () => {
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
+    <>
+    <NavBar/>
+    <div className="hero2">
+        <h1
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          className="text-4xl text-center m-2 p-2 mt-8 font-bold text-white z-10"
+        >
+          Contact Information
+        </h1>
+      </div>
     <div className="container mx-auto p-4 text-center">
-      <h1 className="text-4xl font-extrabold mb-4">Contact Information</h1>
       {loading ? (
         <div>Loading...</div>
       ) : (
         <div className="flex flex-wrap justify-between">
           {currentPosts.map((post) => (
-            <div key={post.id} className="bg-gray-200 p-4 mb-4 w-full">
-              <h2 className="text-xl font-bold mb-2">{post.title}</h2>
-              <p>{post.body}</p>
+            <div key={post.id} className="bg-gray-200 p-4 mb-4 w-[49%]">
+              <div>
+                <div className="flex justify-center items-center">
+                  <i className="fa-solid fa-user mr-2"></i>
+                  <h2 className="text-xl font-bold">{post.name}</h2>
+                </div>
+                <div className="flex justify-center items-center"> 
+                  <i class="fa-solid fa-envelope mr-2"></i>
+                  <p>{post.email}</p>
+                </div>
+                <div className="flex justify-center items-center"> 
+                  <i class="fa-solid fa-phone mr-2"></i>
+                  <p>{post.phone}</p>
+                </div>
+                <div className="flex justify-center items-center"> 
+                  <i class="fa-solid fa-message mr-2"></i>
+                  <p>{post.message}</p>
+                </div>
+              </div>
             </div>
           ))}
         </div>
@@ -45,6 +77,8 @@ const ContactInfo = () => {
         />
       </div>
     </div>
+    <Footer/>
+    </>
   );
 };
 
--- src/components/Enrolled.jsx
+++ src/components/Enrolled.jsx
@@ -1,19 +1,55 @@
 import React, { useState, useEffect } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import NavBar from "./NavBar/NavBar";
+import { Footer } from "antd/es/layout/layout";
 
 const Enrolled = () => {
   const [posts, setPosts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
   const [postsPerPage] = useState(10);
+  const [course, setCourse] = useState("");
 
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
+    window.scroll(0, 0);
+  }, []);
+
+  useEffect(() => {
+    fetch("https://hea-zg7o.onrender.com/courses/all")
       .then((response) => response.json())
       .then((data) => {
-        setPosts(data);
+        console.log(data.courses);
+        setPosts([...data.courses]);
         setLoading(false);
       });
   }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const courseData = { course_name: course };
+    console.log(course);
+    try {
+      const res = await fetch("https://hea-zg7o.onrender.com/courses/add", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(courseData)
+      });
+      const data = await res.json();
+      console.log("data  ", data);
+      if(!data) {
+        console.log("course not added");
+        toast.error("problem in adding course");
+      } else {
+        console.log("course added!!");
+        setCourse("");
+        toast.success("course added successfully!!");
+      }
+    } catch(error) {
+      console.log(error);
+    }
+  }
 
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
@@ -22,16 +58,44 @@ const Enrolled = () => {
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
-    <div className="container mx-auto p-4 text-center">
-      <h1 className="text-4xl font-extrabold mb-4">Enrolled Courses</h1>
-      {loading ? (
+    <>
+    <NavBar/>
+    <div className="hero2">
+        <h1
+          data-aos="fade-up"
+          data-aos-duration="1000"
+          className="text-4xl text-center m-2 p-2 mt-8 font-bold text-white z-10"
+        >
+          Enrolled courses
+        </h1>
+      </div>
+    <div className="container mx-auto p-4 text-center text-black bg-white">
+      <div className="flex my-4">
+        <input
+          type="text"
+          name="name"
+          value={course}
+          className="input input-bordered w-[80%] mr-3 bg-white"
+          onChange={(e) => setCourse(e.target.value)}
+        />
+        <div className="submit">
+          <div className="text-center">
+            <div
+              className="btn bg-primary hover:bg-white text-white border-primary hover:text-primary hover:border-primary text-center shadow-gray-300 shadow-md hover:shadow-2xl"
+              onClick={handleSubmit}
+            >
+              Add Course
+            </div>
+          </div>
+        </div>
+      </div>
+        {loading ? (
         <div>Loading...</div>
       ) : (
         <div className="flex flex-wrap justify-between">
           {currentPosts.map((post) => (
-            <div key={post.id} className="bg-gray-200 p-4 mb-4 w-full">
-              <h2 className="text-xl font-bold mb-2">{post.title}</h2>
-              <p>{post.body}</p>
+            <div key={post._id} className="bg-gray-200 p-4 mb-4 w-[40%]">
+              <h2 className="text-xl font-bold mb-2">{post.course_name}</h2>
             </div>
           ))}
         </div>
@@ -45,6 +109,9 @@ const Enrolled = () => {
         />
       </div>
     </div>
+    <ToastContainer/>
+    <Footer/>
+    </>
   );
 };
 
